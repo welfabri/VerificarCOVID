6b3fee190bb5cef8088caeaa8131f0f2950b485c
welisson <welisson@nucleologuin.com.br>
2021-06-20 20:14:52 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace VerificarCOVID
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblAchouIdade = new System.Windows.Forms.Label();
+            this.tbIdades = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edtTempo = new System.Windows.Forms.NumericUpDown();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.edtTempo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Idades:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 175);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // lblAchouIdade
+            // 
+            this.lblAchouIdade.AutoSize = true;
+            this.lblAchouIdade.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblAchouIdade.ForeColor = System.Drawing.Color.Red;
+            this.lblAchouIdade.Location = new System.Drawing.Point(378, 64);
+            this.lblAchouIdade.Name = "lblAchouIdade";
+            this.lblAchouIdade.Size = new System.Drawing.Size(342, 89);
+            this.lblAchouIdade.TabIndex = 2;
+            this.lblAchouIdade.Text = "ACHOU!!!";
+            this.lblAchouIdade.Visible = false;
+            // 
+            // tbIdades
+            // 
+            this.tbIdades.Location = new System.Drawing.Point(62, 6);
+            this.tbIdades.Name = "tbIdades";
+            this.tbIdades.Size = new System.Drawing.Size(150, 23);
+            this.tbIdades.TabIndex = 3;
+            this.tbIdades.Text = "35,36,37,38,39,40,41";
+            this.toolTip1.SetToolTip(this.tbIdades, "Idades separadas por VÍRGULAS");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Recomeçar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Parar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Minutos:";
+            // 
+            // edtTempo
+            // 
+            this.edtTempo.DecimalPlaces = 1;
+            this.edtTempo.Location = new System.Drawing.Point(290, 6);
+            this.edtTempo.Name = "edtTempo";
+            this.edtTempo.Size = new System.Drawing.Size(51, 23);
+            this.edtTempo.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.edtTempo, "Tempo em Minutos. Necessário RECOMEÇAR para aplicar.");
+            this.edtTempo.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "verificaCOVID_SBC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 212);
+            this.Controls.Add(this.edtTempo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbIdades);
+            this.Controls.Add(this.lblAchouIdade);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "VerifcaCOVID_SBC";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.edtTempo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblAchouIdade;
+        private System.Windows.Forms.TextBox tbIdades;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edtTempo;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace VerificarCOVID
+{
+    public partial class Form1 : Form
+    {
+        private ChromeDriver _driver;
+        private readonly string _url = "https://vacinacovid.saobernardo.sp.gov.br/VacinaSBCProd/servlet/inicial"; //"https://www.random.org/integers/?num=1&min=1&max=100000&col=5&base=10&format=html&rnd=new";
+
+        public Form1()
+        {
+            InitializeComponent();
+            ParametrizaComponentes();
+
+            Iniciar();
+        }
+
+        private void ParametrizaComponentes()
+        {
+            if (_driver is not null)
+                _driver.Quit();
+
+            var path = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+            ChromeOptions options = new();
+            options.AddArgument($"user-data-dir={path}ChromeDriver\\Cache");
+            //options.AddArgument("headless");
+
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            //service.HideCommandPromptWindow = true;
+
+            _driver = new ChromeDriver(service, options);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+            => ProcessaDados();
+
+        private bool AchouIdades(string texto)
+        {
+            var tb = tbIdades.Text.Split(',');
+
+            for (var i = 0; i < tb.Length; i++)
+                if ((!texto.ToUpper().Contains("COMORBIDADE")) 
+                && (texto.Contains(tb[i])))
+                {
+                    texto = "===> ACHOU!!! - " + texto;
+                    lblAchouIdade.Visible = true;
+                    AdicionaTextBox(texto);
+                    return true;
+                }
+
+            return false;
+        }
+
+        private void LimpaControles()
+            => textBox1.Clear();
+
+        private void ProcessaDados()
+        {
+            try
+            {
+                var achou = false;
+
+                _driver.Navigate().Refresh();
+
+                List<IWebElement> selectIdades = _driver.FindElements(By.Id("vPUBLICOALVOID")).ToList();
+
+                foreach (var i in selectIdades)
+                {
+                    var optionsIdade = i.FindElements(By.TagName("option")).ToList();
+
+                    foreach (var o in optionsIdade)
+                    {
+                        achou = AchouIdades(o.Text);
+
+                        if (achou)
+                            break;
+                    }
+                }
+
+                if (!achou)
+                {
+                    AdicionaTextBox("");
+                    VerificarCOVIDUtil.Flash(this, 1);
+                }
+                else
+                {
+                    Parar();
+                    VerificarCOVIDUtil.Flash(this);
+                }
+
+                BringToFront();
+            }
+            catch(Exception e)
+            {
+                AdicionaTextBox(e.Message);
+            }
+        }
+
+        void AdicionaTextBox(string texto)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+                textBox1.Text = textBox1.Text.Insert(0, Environment.NewLine);
+
+            textBox1.Text = textBox1.Text.Insert(0, $"[{DateTime.Now}] " + texto);
+        }
+
+        private void ProcessaDados2()
+        {
+            _driver.Navigate().Refresh();
+            var horario = _driver.FindElement(By.ClassName("data"));
+
+            if (horario is not null)
+                textBox1.AppendText(horario.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+            => Reiniciar();
+
+        private void button2_Click(object sender, EventArgs e)
+            => Parar();
+
+        private void Reiniciar()
+        {
+            Parar();
+            Iniciar();
+        }
+
+        private void Iniciar()
+        {
+            try
+            {
+                LimpaControles();
+                AdicionaTextBox("=====> Iniciando...");
+                _driver.Navigate().GoToUrl(_url);
+                AdicionaTextBox($"=====> Iniciando URL: {_url}");
+                timer1.Interval = decimal.ToInt32(edtTempo.Value * 1000) * 60;
+                timer1.Enabled = true;
+                Timer1_Tick(timer1, null);
+            }
+            catch(Exception e)
+            {
+                AdicionaTextBox($"Erro: {e.Message}");
+                ParametrizaComponentes();
+                AdicionaTextBox("Retentando...");
+                Iniciar();
+            }
+        }
+
+        private void Parar()
+        {
+            AdicionaTextBox("Parando...");
+            timer1.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_driver is not null)
+                _driver.Quit();
+        }
+    }
+}
